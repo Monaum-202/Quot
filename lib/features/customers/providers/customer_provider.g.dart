517e2c29a0c1c30cfa22abcf6322520d137752d6
Monaum-202 <monaum.202@gmail.com>
2026-06-18 00:24:51 +0600

    auto generated file

---
+++ lib/features/customers/providers/customer_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customer_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$customersHash() => r'f757956b0d2f1e033f21a01726371c2c435eec6a';
+
+/// See also [Customers].
+@ProviderFor(Customers)
+final customersProvider =
+    AutoDisposeNotifierProvider<Customers, List<CustomerModel>>.internal(
+  Customers.new,
+  name: r'customersProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$customersHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Customers = AutoDisposeNotifier<List<CustomerModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
